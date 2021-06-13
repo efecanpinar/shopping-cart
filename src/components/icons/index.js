0a9574e4b23c1b9583f7ebac3b7d3f7e5af711c0
Efecan Pınar <48981528+efecanpinar@users.noreply.github.com>
2021-06-13 14:57:38 +0300

    Add icons

---
+++ src/components/icons/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const RefreshIcon = () => (
+  <i className="fa fa-refresh" aria-hidden="true" />
+);
+
+export const RecycleIcon = () => (
+  <i className="fa fa-recycle" aria-hidden="true" />
+);
+
+export const MinusCircleIcon = () => (
+  <i className="fa fa-minus-circle" aria-hidden="true" />
+);
+
+export const PlusCircleIcon = () => (
+  <i className="fa fa-plus-circle" aria-hidden="true" />
+);
+
+export const CartIcon = () => (
+  <i className="fa fa-shopping-cart fa-lg m-2" aria-hidden="true" />
+);
+
+export const TrashIcon = () => (
+  <i className="fa fa-trash-o" aria-hidden="true" />
+);
