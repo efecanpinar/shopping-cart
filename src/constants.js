4b4cffb2a5fb84e4c9e92dfebd7e4749f21e7150
Efecan Pınar <48981528+efecanpinar@users.noreply.github.com>
2021-06-13 14:58:40 +0300

    Add constants for products array

---
+++ src/constants.js
@@ -0,0 +1,56 @@
+export const products = [
+  {
+    id: 0,
+    title: "Vans Sk8-Hi MTE Shoes",
+    subtitle: "Style: VA33TXRJ5",
+    image: "./assets/vans.png",
+    description:
+      "The Vans All-Weather MTE Collection features footwear and apparel designed to withstand the elements whilst still looking cool.",
+    price: 65.0,
+  },
+  {
+    id: 1,
+    title: "The North Face Men Jacket",
+    subtitle: "Model: AVMH_LC9",
+    image: "./assets/man-jacket.png",
+    description:
+      "Developed in partnership with Primaloft, Inc., ThermoBall is a revolution in insulation technology.",
+    price: 90.0,
+  },
+  {
+    id: 2,
+    title: "The North Face Women Jacket",
+    subtitle: "Model: AAVKL_NN4",
+    image: "./assets/women-jacket.png",
+    description:
+      "This waterproof, breathable technology utilizes a polyurethane (PU) coating that consists of a tri-component, multi-layer formula for waterproof protection, moisture permeability and durability",
+    price: 90.0,
+  },
+  {
+    id: 3,
+    title: "Enduro Hydration Pack",
+    subtitle: "Model: AJQZ_JK3",
+    image: "./assets/enrudo-backpack.png",
+    description:
+      "The durable shell features a new graphic along with additional pockets for extra small-item storage.",
+    price: 40.0,
+  },
+  {
+    id: 4,
+    title: "Nike Men Running Shoe",
+    subtitle: "Model: React Infinity",
+    image: "./assets/react-infinity.png",
+    description:
+      "The Nike React Infinity Run Flyknit 2 continues to help keep you running",
+    price: 70.0,
+  },
+  {
+    id: 5,
+    title: "Women Shoe",
+    subtitle: "Model: AXRT_SJ1",
+    image: "./assets/women-shoe.png",
+    description:
+      "From the treadmill to the rowing machine to strength training, you get the amazing energy return of ultralight shoes.",
+    price: 75.0,
+  },
+];
